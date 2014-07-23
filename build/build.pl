85b1bc4d85333108b84f5273327676a66797f6af
davidknott <david.knott@mail.utoronto.ca>
2014-07-22 21:51:15 -0400

    added cover

--- build/build.pl
+++ build/build.pl
@@ -2,6 +2,7 @@
 
 use strict;
 use warnings;
+use File::Copy;
 
 open(BASE, "base.html");
 
@@ -24,44 +25,63 @@ while (<BASE>) {
 }
 
 close(BASE);
-
-
-my $dir = 'projects';
 my $root = '..';
-opendir(DIR, "./$dir") or die $!;
 
-mkdir "$root/$dir";
+sub process_posts { my ($dir) = @_;
+  opendir(DIR, "./$dir") or die $!;
 
-my @index;
+  mkdir "$root/$dir";
 
-while (my $file = readdir(DIR)) {
-    next if $file =~ m/^\./;
-    mkdir "$root/$dir/$file";
-    open(ARTICLE, "./$dir/$file/article.html") or die $!;
-    my @lines = <ARTICLE>;
-    my $article = join '', @lines;
-    close(ARTICLE);
+  my @index;
 
-    my $cover = "";
-    if (-e "./$dir/$file/cover.jpg") {
-      $cover = "<figure style=\"background_image: url('/$dir/$file/cover.jpg')\" ></figure>";
-    }
+  while (my $file = readdir(DIR)) {
+      next if $file =~ m/^\./;
+      mkdir "$root/$dir/$file";
+      open(ARTICLE, "./$dir/$file/article.html") or die $!;
+      my @lines = <ARTICLE>;
+      my $article = join '', @lines;
+      close(ARTICLE);
 
-    my @article_bits = split /\[\[MORE\]\]/, $article;
-    my $index_article = $article_bits[0];
+      my $cover = "";
+      my $cover_class = "no-cover";
+      if (-e "./$dir/$file/cover.jpg") {
+        $cover = "<figure style=\"background-image: url('/$dir/$file/cover.jpg')\" ></figure>";
+        $cover_class = "with-cover";
+        copy("./$dir/$file/cover.jpg", "$root/$dir/$file/cover.jpg") or die $!;
+      }
 
-    $index_article = "<article>\n$cover\n$index_article\n</article>\n";
-    push @index, $index_article;
-    
-    my $main_article = "<article>\n$cover\n$article\n</article>\n";
+      my @article_bits = split /\[\[MORE\]\]/, $article;
+      my $index_article = $article_bits[0];
+      $article = join '', @article_bits;
 
-    open(ARTICLE_MAIN, ">$root/$dir/$file/index.html");
-    print ARTICLE_MAIN $HTML_START;
-    print ARTICLE_MAIN $main_article;
-    print ARTICLE_MAIN $HTML_END;
-    close(ARTICLE_MAIN);
+      $index_article = "<article class=\"index-article $cover_class\">\n$cover\n$index_article\n<a class=\"read-more-link\" href=\"/$dir/$file/\">Read More &rarr;</a>\n</article>\n";
+      push @index, $index_article;
+      
+      my $main_article = "<article class=\"main-article\">\n$cover\n$article\n</article>\n";
 
-    print $index_article;
+      open(ARTICLE_MAIN, ">$root/$dir/$file/index.html") or die $!;
+      print ARTICLE_MAIN $HTML_START;
+      print ARTICLE_MAIN $main_article;
+      print ARTICLE_MAIN $HTML_END;
+      close(ARTICLE_MAIN);
+
+  }
+
+  my $index_full = join '', @index;
+
+  open(ARTICLE_INDEX, ">$root/$dir/index.html") or die $!;
+  print ARTICLE_INDEX $HTML_START;
+  print ARTICLE_INDEX $index_full;
+  print ARTICLE_INDEX $HTML_END;
+  close(ARTICLE_INDEX);
 }
 
-print $/;
+process_posts("projects");
+
+open(INDEX, ">$root/index.html") or die $!;
+print INDEX $HTML_START;
+# print INDEX "";
+print INDEX $HTML_END;
+close(INDEX);
+
+`chmod 755 -R $root`;
